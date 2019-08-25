f847994a2b142468a9ba1f4181365ffc32e51287
greeneye256 <51399139+greeneye256@users.noreply.github.com>
2019-08-25 09:15:37 +0300

    Update Problema2.java

--- src/exercitii/Problema2.java
+++ src/exercitii/Problema2.java
@@ -3,6 +3,6 @@ package exercitii;
 public class Problema2 {
     public static void main(String[] args) {
         int myInt = 123;
-        System.out.println((123%9)*(2*(123%10)));
+        System.out.println((myInt%9)*(2*(myInt%10)));
     }
 }
